a81fccdd15ac90561a981786f4d1578b5ab1fd7e
Afsah ur Rehman <afsahurrehman11@gmail.com>
2024-10-10 03:12:00 +0500

    added js for components, and login page, and other assets added

---
+++ src/categories.js
@@ -0,0 +1,89 @@
+
+
+const categories = [
+
+    {
+        id: 2,
+        name: "Top Cities",
+        url: 'https://a0.muscache.com/pictures/ed8b9e47-609b-44c2-9768-33e6a22eccb2.jpg'
+    },
+    {
+        id: 3,
+        name: "Amazing Views",
+        url: 'https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg'
+    },
+    {
+        id: 4,
+        name: "Bed & breakfast",
+        url: 'https://a0.muscache.com/pictures/5ed8f7c7-2e1f-43a8-9a39-4edfc81a3325.jpg'
+    },
+    {
+        id: 5,
+        name: "Camping",
+        url: 'https://a0.muscache.com/pictures/ca25c7f3-0d1f-432b-9efa-b9f5dc6d8770.jpg'
+    },
+    {
+        id: 6,
+        name: "Cabins",
+        url: 'https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg'
+    },
+    {
+        id: 7,
+        name: "Rooms",
+        url: 'https://a0.muscache.com/pictures/7630c83f-96a8-4232-9a10-0398661e2e6f.jpg'
+    },
+    {
+        id: 8,
+        name: "Mansions",
+        url: 'https://a0.muscache.com/pictures/78ba8486-6ba6-4a43-a56d-f556189193da.jpg'
+    },
+    {
+        id: 9,
+        name: "Castles",
+        url: 'https://a0.muscache.com/pictures/1b6a8b70-a3b6-48b5-88e1-2243d9172c06.jpg'
+    },
+    {
+        id: 10,
+        name: "OMG!",
+        url: 'https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg'
+    },
+    {
+        id: 11,
+        name: "Trendings",
+        url: 'https://a0.muscache.com/pictures/3726d94b-534a-42b8-bca0-a0304d912260.jpg'
+    },
+    {
+        id: 12,
+        name: "Arctic",
+        url: 'https://a0.muscache.com/pictures/8b44f770-7156-4c7b-b4d3-d92549c8652f.jpg'
+    },
+    {
+        id: 13,
+        name: "New",
+        url: 'https://a0.muscache.com/pictures/c0fa9598-4e37-40f3-b734-4bd0e2377add.jpg'
+    },
+    {
+        id: 14,
+        name: "Countryside",
+        url: 'https://a0.muscache.com/pictures/6ad4bd95-f086-437d-97e3-14d12155ddfe.jpg'
+    },
+    {
+        id: 15,
+        name: "Luxe",
+        url: 'https://a0.muscache.com/pictures/c8e2ed05-c666-47b6-99fc-4cb6edcde6b4.jpg'
+    },
+    {
+        id: 16,
+        name: "Tropical",
+        url: 'https://a0.muscache.com/pictures/ee9e2a40-ffac-4db9-9080-b351efc3cfc4.jpg'
+    },
+    {
+        id: 17,
+        name: "Island",
+        url: 'https://a0.muscache.com/pictures/8e507f16-4943-4be9-b707-59bd38d56309.jpg'
+    },
+
+
+];
+
+export default categories;
